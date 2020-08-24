d6730e2141aaa40342e2b832b9f4ea2525b36740
Brooklyn Hunt Craven <brooklynhuntcraven@Brooklyns-MacBook-Pro.local>
2020-08-23 18:23:29 -0700

    computer switch

---
+++ Develop/db/schema.sql
@@ -0,0 +1,21 @@
+-- DROP DATABASE
+DROP DATABASE IF EXISTS ecommerce_db;
+
+-- CREATE DATABASE
+CREATE DATABASE ecommerce_db;
+
+CREATE TABLE catagory{
+
+}
+
+CREATE TABLE product{
+
+}
+
+CREATE TABLE productTag{
+
+}
+
+CREATE TABLE tag{
+
+}
